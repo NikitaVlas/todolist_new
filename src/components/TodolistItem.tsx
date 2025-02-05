c9f11b6de7ac61409acb3c6505c8b22766569d2d
NikitaVlas <nikistosov@gmail.com>
2025-02-05 12:42:30 +0100

    feat: map tasks

--- src/components/TodolistItem.tsx
+++ src/components/TodolistItem.tsx
@@ -14,15 +14,12 @@ const TodolistItem = ({title, tasks}: TodolistItemProps) => {
                 <button>+</button>
             </div>
             <ul>
-                <li>
-                    <input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span>
-                </li>
+                {tasks.map(m => (
+                    <li key={m.id}>
+                        <input type="checkbox" checked={m.isDone}/>
+                        <span>{m.title}</span>
+                    </li>
+                ))}
             </ul>
             <div>
                 <button>All</button>

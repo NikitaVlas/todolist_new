f7c258ee8d7b9138c27a4a551af7ce62e967fbb7
NikitaVlas <nikistosov@gmail.com>
2025-02-20 20:08:18 +0100

    feat: reducer added

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,8 @@
+import {Todolist} from "../App.tsx";
+
+
+const initialState: Todolist[] = []
+
+export const todolistsReducer = (state: Todolist[] = initialState, action: any): Todolist[] => {
+
+}

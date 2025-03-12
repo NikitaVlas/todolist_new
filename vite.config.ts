43b7f81531562186280e9be71a09ce98e259228f
NikitaVlas <nikistosov@gmail.com>
2025-03-12 17:32:02 +0100

    fix: localhost

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Меняем хост (например, на локальную сеть)
+    port: 3001, // Можно изменить порт, если нужно
+  },
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
